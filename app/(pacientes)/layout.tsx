d0a544361837018bdd64ffac219893057967f8bd
Emaa27 <gefrias.27@gmail.com>
2025-09-25 23:56:44 -0300

    entorno v1.1

--- app/(pacientes)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import ReceptionSidebarLayout from '../../components/SidebarLayout';
-
-export default function PacientesLayout({ children }: { children: React.ReactNode }) {
-  return <ReceptionSidebarLayout>{children}</ReceptionSidebarLayout>;
-}
